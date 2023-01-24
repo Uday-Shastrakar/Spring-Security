adfc27a8dbf3166c55a3ed8d7e1b07452d165506
Uday Shastrakar <uday.shastrakar@gmail.com>
2023-01-24 22:37:58 +0530

    user and password encoder

--- security/src/main/java/com/app/config/SecurityConfig.java
+++ security/src/main/java/com/app/config/SecurityConfig.java
@@ -3,26 +3,38 @@ package com.app.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 	
+	public PasswordEncoder passwordEncoder() {
+		return new BcryptPasswordEncoder();
+	}
+
 	@Bean
 	public SecurityFilterChain filterchain(HttpSecurity httpsecurity) throws Exception {
-		
-		httpsecurity.csrf().disable()
-		.authorizeHttpRequests()
-		.requestMatchers("/home/public")
-		.permitAll()
-		.anyRequest()
-		.authenticated()
-		.and()
-		.formLogin();
-		
+
+		httpsecurity.csrf().disable().authorizeHttpRequests().requestMatchers("/home/public").permitAll().anyRequest()
+				.authenticated().and().formLogin();
+
 		return httpsecurity.build();
-		
-		
+
+	}
+    @Bean
+	public UserDetailsService userDetailsService() {
+        UserDetails normaluser = User
+        		.withUsername("uday")
+        		.password("password")
+        		.roles("ROLE_NORMAL")
+        		.build();
+        
+        return new InMemoryUserDetailsManager(normaluser);
 	}
 
 }
